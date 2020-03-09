2ba4865c3033b6ffdc050937d2a94221a90c88f8
Amr <amratya01@gmail.com>
2020-03-09 17:02:59 +0100

    removing condition on testEnd function

--- src/Yandex/Allure/Codeception/AllureCodeception.php
+++ src/Yandex/Allure/Codeception/AllureCodeception.php
@@ -387,7 +387,7 @@ class AllureCodeception extends Extension
     public function testEnd(TestEvent $testEvent)
     {
         // attachments supported since Codeception 3.0
-        if (version_compare(Codecept::VERSION, '3.0.0') > -1 && $testEvent->getTest() instanceof Cest) {
+        if (version_compare(Codecept::VERSION, '3.0.0') > -1) {
             $artifacts = $testEvent->getTest()->getMetadata()->getReports();
             $testCaseStorage = $this->getLifecycle()->getTestCaseStorage()->get();
             foreach ($artifacts as $name => $artifact) {
